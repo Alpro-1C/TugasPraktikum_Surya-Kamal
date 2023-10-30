7551879a7cf6d07fa4d3b56dd48e9950e5cbb323
Surya Kamal <89984966+Kamal1202@users.noreply.github.com>
2023-10-30 09:57:43 +0700

    Update Tugas3-1.cpp

--- Pertemuan-3/Tugas3-1.cpp
+++ Pertemuan-3/Tugas3-1.cpp
@@ -25,7 +25,7 @@ int main(){
     cout << endl;
 
     //Program if yang akan mengeluarkan output "Selamat, anda mendapatkan nilai A" jika nilai lebih dari sama dengan 90
-    if (nilai >= 90){
+    if (nilai >= 90 && nilai <= 100){
         cout << "Selamat!, Anda mendapatkan nilai A \n\n";
     }
     //Program else-if yang akan mengeluarkan output "Anda mendapatkan nilai B" jika nilai lebih dari sama dengan 80 dan nilai kurang dari sama dengan 89.
@@ -41,7 +41,7 @@ int main(){
         cout << "Anda mendapatkan nilai D \n\n";
     }
     //Program else-if yang akan mengeluarkan output "Anda mendapatkan nilai E" jika nilai kurang dari 60.
-    else if (nilai < 60){
+    else if (nilai <= 59 && nilai >=0){
         cout << "Anda mendapatkan nilai E \n\n";
     }
     //Program else untuk menangkap input yang tidak sesuai dari semua kondisi di atas
